4844cb40411c46de9543fb773ebd8b5f30910e14
IceDice <dice7912@naver.com>
2021-05-08 11:04:47 +0900

    asdfsadf

--- src/main/resources/static/js/ProductList/ProdList.js
+++ src/main/resources/static/js/ProductList/ProdList.js
@@ -55,13 +55,15 @@ $(document).ready(function(){
 					let sumMsg='';
 					var i;
 					for (i=0; i<my_array.length; i++){
-						sumMsg += "<ul class='prod_li'> <li> <div class='prodSet'> <p class='prod_img'> <span class='prod_imgSet'> <img src='" + my_array[i].book.imgurl 
-								+ "border='0'> </a>	</span> </p> </div> <div class='prod_info'> <div class='prod_name'> <a>"
-								+ my_array[i].book.title + "</a><br><br> </div> <div class='prod_pubGrp'> <span class='prod_auth'>" 
-								+ my_array[i].book.author + "</a> 저 	</span> <em class='divi'>｜</em> <span class='prod_pub'>"
-								+ my_array[i].book.publisher + "</span> <em class='divi'>｜</em> </div> <div class='prod_price'> <em class='price'>" 
-								+ my_array[i].book.price + "</em> 원 </div> </div> <div class='prod_btn'> <a href='#' class='btn1'>카트에 넣기</a><a class='btn2' href='#' onclick=alert("+my_array[i].book.isbn+")+setCookie("+my_array[i].book.isbn+","+1+")+window.open('../../html/ProductDetail/ProductDetail.html')>바로 구매하기</a></div></li></ul>";
-					}
+						sumMsg += "<ul class='prod_li'> <li> <div class='prodSet'> <p class='prod_img'> <span class='prod_imgSet'> <a href='#'> <img src='" + my_array[i].book.imgurl 
+						+ "border='0'> </a>	</span> </p> </div> <div class='prod_info'> <div class='prod_name'> <a>"
+						+ my_array[i].book.title + "</a><br><br> </div> <div class='prod_pubGrp'> <span class='prod_auth'>" 
+						+ my_array[i].book.author + "</a> 저 	</span> <em class='divi'>｜</em> <span class='prod_pub'>"
+						+ my_array[i].book.publisher + "</span> <em class='divi'>｜</em> </div> <div class='prod_price'> <em class='price'>" 
+						+ my_array[i].book.price + "</em> 원 </div> </div> <div class='prod_btn'> <a href='#' class='btn1' onclick=\"window.open('../../html/OrderPage/orderForm.html', '_blank', 'toolbar=yes,scrollbars=yes,resizable=yes,top=50,left=500,width=600,height=600');\">카트에 넣기</a><a class='btn2' href='#' onclick=alert("
+						+ my_array[i].book.isbn+")+setCookie("
+						+ my_array[i].book.isbn+","+1+")+window.open('../../html/ProductDetail/ProductDetail.html')>바로 구매하기</a></div></li></ul>";		
+						}
 					$('#prod_list').html(sumMsg);
 						
 				});	
@@ -88,13 +90,15 @@ $(document).ready(function(){
 					let sumMsg='';
 					var i;
 					for (i=0; i<my_array.length; i++){
-						sumMsg += "<ul class='prod_li'> <li> <div class='prodSet'> <p class='prod_img'> <span class='prod_imgSet'> <img src='" + my_array[i].book.imgurl 
-								+ "border='0'> </a>	</span> </p> </div> <div class='prod_info'> <div class='prod_name'> <a>"
-								+ my_array[i].book.title + "</a><br><br> </div> <div class='prod_pubGrp'> <span class='prod_auth'>" 
-								+ my_array[i].book.author + "</a> 저 	</span> <em class='divi'>｜</em> <span class='prod_pub'>"
-								+ my_array[i].book.publisher + "</span> <em class='divi'>｜</em> </div> <div class='prod_price'> <em class='price'>" 
-								+ my_array[i].book.price + "</em> 원 </div> </div> <div class='prod_btn'> <a href='#' class='btn1'>카트에 넣기</a><a class='btn2' href='#' onclick=alert("+my_array[i].book.isbn+")+setCookie("+my_array[i].book.isbn+","+1+")+window.open('../../html/ProductDetail/ProductDetail.html')>바로 구매하기</a></div></li></ul>";
-					}
+						sumMsg += "<ul class='prod_li'> <li> <div class='prodSet'> <p class='prod_img'> <span class='prod_imgSet'> <a href='#'> <img src='" + my_array[i].book.imgurl 
+						+ "border='0'> </a>	</span> </p> </div> <div class='prod_info'> <div class='prod_name'> <a>"
+						+ my_array[i].book.title + "</a><br><br> </div> <div class='prod_pubGrp'> <span class='prod_auth'>" 
+						+ my_array[i].book.author + "</a> 저 	</span> <em class='divi'>｜</em> <span class='prod_pub'>"
+						+ my_array[i].book.publisher + "</span> <em class='divi'>｜</em> </div> <div class='prod_price'> <em class='price'>" 
+						+ my_array[i].book.price + "</em> 원 </div> </div> <div class='prod_btn'> <a href='#' class='btn1' onclick=\"window.open('../../html/OrderPage/orderForm.html', '_blank', 'toolbar=yes,scrollbars=yes,resizable=yes,top=50,left=500,width=600,height=600');\">카트에 넣기</a><a class='btn2' href='#' onclick=alert("
+						+ my_array[i].book.isbn+")+setCookie("
+						+ my_array[i].book.isbn+","+1+")+window.open('../../html/ProductDetail/ProductDetail.html')>바로 구매하기</a></div></li></ul>";		
+						}
 					$('#prod_list').html(sumMsg);
 						
 				});		
