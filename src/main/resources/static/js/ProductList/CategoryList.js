16b40705ce97f64c912f0eb846563fca96e2a193
IceDice <dice7912@naver.com>
2021-05-08 10:35:40 +0900

    chat UI

--- src/main/resources/static/js/ProductList/CategoryList.js
+++ src/main/resources/static/js/ProductList/CategoryList.js
@@ -19,12 +19,13 @@ $(document).ready(function(){
 			let sumMsg='';
 			for (i=0; i<my_array.length; i++){
 				sumMsg += "<ul class='prod_li'> <li> <div class='prodSet'> <p class='prod_img'> <span class='prod_imgSet'> <a href='#'> <img src='" + my_array[i].book.imgurl 
-						+ "border='0'> </a>	</span> </p> </div> <div class='prod_info'> <div class='prod_name'> <a>"
-						+ my_array[i].book.title + "</a><br><br> </div> <div class='prod_pubGrp'> <span class='prod_auth'>" 
-						+ my_array[i].book.author + "</a> 저 	</span> <em class='divi'>｜</em> <span class='prod_pub'>"
-						+ my_array[i].book.publisher + "</span> <em class='divi'>｜</em> </div> <div class='prod_price'> <em class='price'>" 
-						+ my_array[i].book.price + "</em> 원 </div> </div> <div class='prod_btn'> <a href='#' class='btn1'>카트에 넣기</a><a class='btn2' href='#' onclick=alert("+my_array[i].book.isbn+")+setCookie("+my_array[i].book.isbn+","+1+")+window.open('../../html/ProductDetail/ProductDetail.html')>바로 구매하기</a></div></li></ul>";
-			
+				+ "border='0'> </a>	</span> </p> </div> <div class='prod_info'> <div class='prod_name'> <a>"
+				+ my_array[i].book.title + "</a><br><br> </div> <div class='prod_pubGrp'> <span class='prod_auth'>" 
+				+ my_array[i].book.author + "</a> 저 	</span> <em class='divi'>｜</em> <span class='prod_pub'>"
+				+ my_array[i].book.publisher + "</span> <em class='divi'>｜</em> </div> <div class='prod_price'> <em class='price'>" 
+				+ my_array[i].book.price + "</em> 원 </div> </div> <div class='prod_btn'> <a href='#' class='btn1' onclick=\"window.open('../../html/OrderPage/orderForm.html', '_blank', 'toolbar=yes,scrollbars=yes,resizable=yes,top=50,left=500,width=600,height=600');\">카트에 넣기</a><a class='btn2' href='#' onclick=alert("
+				+ my_array[i].book.isbn+")+setCookie("
+				+ my_array[i].book.isbn+","+1+")+window.open('../../html/ProductDetail/ProductDetail.html')>바로 구매하기</a></div></li></ul>";	
 //				alert(i+"번째 책 isbn : " + my_array[i].book.isbn);
 				
 			}
